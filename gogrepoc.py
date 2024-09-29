97b602ef767dfc6e165bf8da61b82026a7966064
Matt Passarelli <mattvpassarelli@gmail.com>
2024-09-29 11:48:47 -0400

    restarted with better boilerplate template

--- gogrepoc.py
+++ gogrepoc.py
@@ -5416,33 +5416,105 @@ def cmd_clean(cleandir, dryrun):
         info("nothing to clean. nice and tidy!")
 
 
-def item_row(item_num):
-    row = sg.T(item_num)
-    return row
+def get_theme():
+    """
+    Get the theme to use for the program
+    Value is in this program's user settings. If none set, then use PySimpleGUI's global default theme
+    :return: The theme
+    :rtype: str
+    """
+    # First get the current global theme for PySimpleGUI to use if none has been set for this program
+    try:
+        global_theme = sg.theme_global()
+    except:
+        global_theme = sg.theme()
+    # Get theme from user settings for this program.  Use global theme if no entry found
+    user_theme = sg.user_settings_get_entry('-theme-', '')
+    if user_theme == '':
+        user_theme = global_theme
+    return user_theme
+
+
+def get_game_list():
+    print("getting list")
+    all_games = load_manifest()
+    game_list = []
+    for game in all_games:
+        print(game.title)
+        game_list.append(game.title)
+    
+    return sorted(game_list)
 
 
 def cmd_gui(args):
+    ML_KEY = '-ML-'
+
+    theme = get_theme()
+    if not theme:
+        theme = sg.OFFICIAL_PYSIMPLEGUI_THEME
+    sg.theme(theme)
+
+    find_tooltip = "Find in file\nEnter a string in box to search for string inside of the files.\nFile list will update with list of files string found inside."
+    filter_tooltip = "Filter files\nEnter a string in box to narrow down the list of files.\nFile list will update with list of files with string in filename."
+    find_re_tooltip = "Find in file using Regular Expression\nEnter a string in box to search for string inside of the files.\nSearch is performed after clicking the FindRE button."
+
+
+    left_col = sg.Col([
+        [sg.Listbox(values=get_game_list(), select_mode=sg.SELECT_MODE_EXTENDED, size=(50, 20), key='-DEMO LIST-')],
+        [sg.Text('Filter:', tooltip=filter_tooltip), sg.Input(size=(25, 1), enable_events=True, key='-FILTER-', tooltip=filter_tooltip),
+         sg.T(size=(15,1), k='-FILTER NUMBER-')],
+        [sg.Button('Run'), sg.B('Edit'), sg.B('Clear'), sg.B('Open Folder')],
+        [sg.Text('Find:', tooltip=find_tooltip), sg.Input(size=(25, 1), enable_events=True, key='-FIND-', tooltip=find_tooltip),
+         sg.T(size=(15,1), k='-FIND NUMBER-')],
+    ], element_justification='l')
+
+    lef_col_find_re = sg.pin(sg.Col([
+        [sg.Text('Find:', tooltip=find_re_tooltip), sg.Input(size=(25, 1),key='-FIND RE-', tooltip=find_re_tooltip),sg.B('Find RE')]], k='-RE COL-'))
+
+    right_col = [
+        [sg.Multiline(size=(70, 21), write_only=True, key=ML_KEY, reroute_stdout=True, echo_stdout_stderr=True)],
+        [sg.Button('Edit Me (this program)'), sg.B('Settings'), sg.Button('Exit')],
+        [sg.T('PySimpleGUI ver ' + sg.version.split(' ')[0] + '  tkinter ver ' + sg.tclversion_detailed, font='Default 8', pad=(0,0))],
+    ]
+
+    options_at_bottom = sg.pin(sg.Column([[sg.CB('Verbose', enable_events=True, k='-VERBOSE-'),
+                         sg.CB('Show only first match in file', default=True, enable_events=True, k='-FIRST MATCH ONLY-'),
+                         sg.CB('Find ignore case', default=True, enable_events=True, k='-IGNORE CASE-'),
+                         sg.T('<---- NOTE: Only the "Verbose" setting is implemented at this time')]], pad=(0,0), k='-OPTIONS BOTTOM-'))
+
+    choose_folder_at_top = sg.pin(sg.Column([[sg.T('Click settings to set top of your tree or choose a previously chosen folder'),
+                                       sg.Combo(sorted(sg.user_settings_get_entry('-folder names-', [])), default_value=sg.user_settings_get_entry('-demos folder-', ''), size=(50, 1), key='-FOLDERNAME-', enable_events=True, readonly=True)]], pad=(0,0), k='-FOLDER CHOOSE-'))
+    # ----- Full layout -----
+
+    layout = [[sg.Text('PySimpleGUI Demo Program & Project Browser', font='Any 20')],
+              [choose_folder_at_top],
+              sg.vtop([sg.Column([[left_col],[ lef_col_find_re]], element_justification='l'), sg.Col(right_col, element_justification='c') ]),
+              [options_at_bottom]
+              ]
+
+    # --------------------------------- Create Window ---------------------------------
+
+
     # All the stuff inside your window.
-    layout = []
     try:
         makeGOGSession()
-        layout = [
-            [
-                sg.Text(
-                    "Click the Update button below to populate the list with your GOG Games",
-                    font="_ 15",
-                )
-            ],
-            [sg.Col([], k="-TRACKING SECTION-")],
-            [
-                sg.Button(
-                    "Update List",
-                    enable_events=True,
-                    k="Update List",
-                    tooltip="Populate Game List",
-                )
-            ],
-        ]
+        # layout = [
+        #     [
+        #         sg.Text(
+        #             "Click the Update button below to populate the list with your GOG Games",
+        #             font="_ 15",
+        #         )
+        #     ],
+        #     [sg.Col([], k="-TRACKING SECTION-")],
+        #     [
+        #         sg.Button(
+        #             "Update List",
+        #             enable_events=True,
+        #             k="Update List",
+        #             tooltip="Populate Game List",
+        #         )
+        #     ],
+        # ]
 
     except (KeyError, AttributeError):
         layout = [
@@ -5455,13 +5527,7 @@ def cmd_gui(args):
         ]
 
     # Create the Window
-    window = sg.Window("GOG Downloader", layout, metadata=0)
-
-    all_games = load_manifest()
-    for game in all_games:
-        print(game.title)
-        window.metadata += 1
-        window.extend_layout(window["-TRACKING SECTION-"], [item_row(game.title)])
+    window = sg.Window('PSG Demo & Project Browser', layout, finalize=True, metadata=0)
 
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
